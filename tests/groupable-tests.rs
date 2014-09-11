a6a2f1f080816887275e6ebf485a28b035762597
Kevin Butler <haqkrs@gmail.com>
2014-09-11 22:38:21 +0100

    Add tests grouping into Vec.

--- tests/groupable-tests.rs
+++ tests/groupable-tests.rs
@@ -1,3 +1,4 @@
+#![feature(macro_rules)]
 extern crate groupable;
 
 use std::collections::{HashMap, TreeMap, SmallIntMap, TrieMap};
@@ -5,42 +6,52 @@ use groupable::Groupable;
 
 static XS : &'static [char] = ['h', 'b', 'i', 'y', '!', 'e'];
 
+macro_rules! test_string (
+    ($t:ty) => (
+        {
+            let map = XS.iter().enumerate()
+                           .map(|(i, &c)| (i % 2, c))
+                           .group::<$t>();
+
+            assert_eq!(map[0].as_slice(), "hi!");
+            assert_eq!(map[1].as_slice(), "bye");
+        }
+    )
+)
+
+macro_rules! test_char_vec (
+    ($t:ty) => (
+        {
+            let map = XS.iter().enumerate()
+                           .map(|(i, &c)| (i % 2, c))
+                           .group::<$t>();
+
+            assert_eq!(map[0].as_slice(), ['h', 'i', '!'].as_slice());
+            assert_eq!(map[1].as_slice(), ['b', 'y', 'e'].as_slice());
+        }
+    )
+)
+
 #[test]
 fn hashmap() {
-    let map = XS.iter().enumerate()
-                       .map(|(i, &c)| (i % 2, c))
-                       .group::<HashMap<uint, String>>();
-
-    assert_eq!(map[0].as_slice(), "hi!");
-    assert_eq!(map[1].as_slice(), "bye");
+    test_string!(HashMap<uint, String>)
+    test_char_vec!(HashMap<uint, Vec<char>>)
 }
 
 #[test]
 fn treemap() {
-    let map = XS.iter().enumerate()
-                       .map(|(i, &c)| (i % 2, c))
-                       .group::<TreeMap<uint, String>>();
-
-    assert_eq!(map[0].as_slice(), "hi!");
-    assert_eq!(map[1].as_slice(), "bye");
+    test_string!(TreeMap<uint, String>)
+    test_char_vec!(TreeMap<uint, Vec<char>>)
 }
 
 #[test]
 fn smallintmap() {
-    let map = XS.iter().enumerate()
-                       .map(|(i, &c)| (i % 2, c))
-                       .group::<SmallIntMap<String>>();
-
-    assert_eq!(map[0].as_slice(), "hi!");
-    assert_eq!(map[1].as_slice(), "bye");
+    test_string!(SmallIntMap<String>)
+    test_char_vec!(SmallIntMap<Vec<char>>)
 }
 
 #[test]
 fn triemap() {
-    let map = XS.iter().enumerate()
-                       .map(|(i, &c)| (i % 2, c))
-                       .group::<TrieMap<String>>();
-
-    assert_eq!(map[0].as_slice(), "hi!");
-    assert_eq!(map[1].as_slice(), "bye");
+    test_string!(TrieMap<String>)
+    test_char_vec!(TrieMap<Vec<char>>)
 }
