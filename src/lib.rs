ffa890b4b6ca9623a05f0e469911b99956ed0a8f
Kevin Butler <haqkrs@gmail.com>
2014-09-11 22:29:15 +0100

    Implement for SmallIntMap and TrieMap

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(macro_rules)]
-use std::collections::{HashMap, TreeMap};
+use std::collections::{HashMap, TreeMap, SmallIntMap, TrieMap};
 use std::hash::Hash;
 
 pub trait Groupable<K, V> {
@@ -16,23 +16,44 @@ trait FromKeyedIterator<K, V> {
     fn from_keyed_iter<I: Iterator<(K, V)>>(I) -> Self;
 }
 
+#[inline(always)]
+fn group_into<K,
+              V,
+              U: Extendable<V>,
+              I: Iterator<(K, V)>,
+              M: MutableMap<K, U>>(mut iter: I, map: &mut M) {
+    for (key, val) in iter {
+        let val_iter = Some(val).move_iter();
+        match map.find_mut(&key) {
+            Some(collection) => {
+                collection.extend(val_iter);
+                continue
+            },
+            None => {} // insert below
+        }
+
+        map.insert(key, FromIterator::from_iter(val_iter));
+    }
+}
+
 macro_rules! impl_keyed_iter (
     ($name:ident: $($bounds:ident),+) => (
         impl <K: $($bounds+)+, V, U: Extendable<V>> FromKeyedIterator<K, V> for $name<K, U> {
-            fn from_keyed_iter<T: Iterator<(K, V)>>(mut iter: T) -> $name<K, U> {
+            fn from_keyed_iter<T: Iterator<(K, V)>>(iter: T) -> $name<K, U> {
                 let mut map = $name::<K, U>::new();
-                    for (key, val) in iter {
-                    let val_iter = Some(val).move_iter();
-                    match map.find_mut(&key) {
-                        Some(collection) => {
-                            collection.extend(val_iter);
-                            continue
-                        },
-                        None => {} // insert below
-                    }
+                group_into(iter, &mut map);
+                map
+            }
+        }
+    )
+)
 
-                    map.insert(key, FromIterator::from_iter(val_iter));
-                }
+macro_rules! impl_uint_keyed_iter (
+    ($name:ident) => (
+        impl <V, U: Extendable<V>> FromKeyedIterator<uint, V> for $name<U> {
+            fn from_keyed_iter<T: Iterator<(uint, V)>>(iter: T) -> $name<U> {
+                let mut map = $name::<U>::new();
+                group_into(iter, &mut map);
                 map
             }
         }
@@ -41,3 +62,5 @@ macro_rules! impl_keyed_iter (
 
 impl_keyed_iter!(HashMap: Ord, Hash)
 impl_keyed_iter!(TreeMap: Ord)
+impl_uint_keyed_iter!(SmallIntMap)
+impl_uint_keyed_iter!(TrieMap)
--- tests/groupable-tests.rs
+++ tests/groupable-tests.rs
@@ -1,6 +1,6 @@
 extern crate groupable;
 
-use std::collections::{HashMap, TreeMap};
+use std::collections::{HashMap, TreeMap, SmallIntMap, TrieMap};
 use groupable::Groupable;
 
 static XS : &'static [char] = ['h', 'b', 'i', 'y', '!', 'e'];
@@ -20,6 +20,26 @@ fn treemap() {
     let map = XS.iter().enumerate()
                        .map(|(i, &c)| (i % 2, c))
                        .group::<TreeMap<uint, String>>();
+
+    assert_eq!(map[0].as_slice(), "hi!");
+    assert_eq!(map[1].as_slice(), "bye");
+}
+
+#[test]
+fn smallintmap() {
+    let map = XS.iter().enumerate()
+                       .map(|(i, &c)| (i % 2, c))
+                       .group::<SmallIntMap<String>>();
+
+    assert_eq!(map[0].as_slice(), "hi!");
+    assert_eq!(map[1].as_slice(), "bye");
+}
+
+#[test]
+fn triemap() {
+    let map = XS.iter().enumerate()
+                       .map(|(i, &c)| (i % 2, c))
+                       .group::<TrieMap<String>>();
 
     assert_eq!(map[0].as_slice(), "hi!");
     assert_eq!(map[1].as_slice(), "bye");
